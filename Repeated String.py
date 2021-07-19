7ba7fd4decbbc32c9e1c63c4f165f68fc16e10f8
Impact-coder <76546289+Impact-coder@users.noreply.github.com>
2021-07-19 19:34:08 +0530

    Add files via upload

---
+++ Repeated String.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'repeatedString' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. LONG_INTEGER n
+#
+
+def repeatedString(s, n):
+    # Write your code here
+    x=len(s)
+    y=s.count("a")
+    count=(n//x)*y
+    mod  = n%x
+    
+    for i in range(0,mod):
+        if (s[i]=='a'):
+            count+=1
+            
+    return count
+             
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input().strip())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
